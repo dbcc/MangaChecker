a618fbc279abc4d2bfa4d0e9e638774fe8f34719
tensei <davuttensei@gmail.com>
2016-07-31 07:15:16 +0200

    first part of import from old project

---
+++ MangaChecker.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MangaChecker.ViewModels.dependencies;
+
+namespace MangaChecker.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+    }
+}
